f3cc812ed5cbc5ae6d1bafc15a5bb216120e65a2
Reporterxxx <mikhailbiruykov@mail.ru>
2023-03-04 13:35:12 +0300

    Refactor file LoginDataSource.kt
    Changes: one input - one output

--- app/src/main/java/com/example/not_last_dish/data/LoginDataSource.kt
+++ app/src/main/java/com/example/not_last_dish/data/LoginDataSource.kt
@@ -9,13 +9,22 @@ import java.io.IOException
 class LoginDataSource {
 
     fun login(username: String, password: String): Result<LoggedInUser> {
-        try {
+//        try {
+//            // TODO: handle loggedInUser authentication
+//            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+//            return Result.Success(fakeUser)
+//        } catch (e: Throwable) {
+//            return Result.Error(IOException("Error logging in", e))
+//        }
+//        Structure rule one input - one exit
+        val result: Result<LoggedInUser> = try {
             // TODO: handle loggedInUser authentication
             val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
-            return Result.Success(fakeUser)
+            Result.Success(fakeUser)
         } catch (e: Throwable) {
-            return Result.Error(IOException("Error logging in", e))
+            Result.Error(IOException("Error logging in", e))
         }
+        return result
     }
 
     fun logout() {
